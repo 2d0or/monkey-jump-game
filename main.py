0400731a0421798bc5a9014b803ea8a493661d01
2door <tudorcazacu85@gmail.com>
2023-05-18 23:21:25 +0200

    new background

--- main.py
+++ main.py
@@ -73,12 +73,17 @@ base1=Base(base_width,base_height,base_x_2,base_y_1)
 base2=Base(base_width,base_height, base_x_2, base_y_2)
 
 
+all_sprites = pygame.sprite.Group()
 
 #initialize pygame
 pygame.init()
 
 #colco
 clock = pygame.time.Clock()
+
+#background
+background_image = pygame.image.load("sky.jpg")
+background_y = 0
 
 
 #game conditions 
@@ -90,6 +95,8 @@ while True:
     clock.tick(60)
 
 
+
+    
 
     #--------
     # introduce player collision with base & gravits
@@ -152,13 +159,22 @@ while True:
 
     ## introduce jump mechanics
     if keys[pygame.K_SPACE]:
+        # Scroll the background
+        background_y += 5
+
+        if background_y >= screen_height:
+            background_y = 0
+
+    # Draw background
+       
         player.rect.y -=8
 
 
 
+    screen.blit(background_image, (0, background_y))
+    screen.blit(background_image, (0, background_y - screen_height))
 
-
-    screen.fill((255,255,255))
+    #screen.fill((255,255,255))
 
     screen.blit(player.image,player.rect)
     screen.blit(base.image, base.rect)
@@ -169,7 +185,7 @@ while True:
     base1.update()
     base2.update()
 
-    #all_sprites.update()
-    #all_sprites.draw(screen)
+    all_sprites.update()
+    all_sprites.draw(screen)
 
     pygame.display.update()
